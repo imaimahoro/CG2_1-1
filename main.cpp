2804ac79dd0666aa543a0896ef8478ce33317678
imai-mahoro <k021g1253@g.neec.ac.jp>
2022-06-01 11:25:12 +0900

    CG2-05_03　射影変換

--- main.cpp
+++ main.cpp
@@ -290,10 +290,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	//頂点データ
 	Vertex vertices[] =
 	{
-		{{ 0.0f,100.0f,0.0f},   {0.0f,1.0f}},//左下 インデックス0
-		{{ 0.0f,0.0f,0.0f},     {0.0f,0.0f}},//左上 インデックス1
-		{{ 100.0f,100.0f,0.0f}, {1.0f,1.0f}},//右下 インデックス2
-		{{ 100.0f,0.0f,0.0f},   {1.0f,0.0f}},//右上 インデックス3
+		{{ -50.0f,-50.0f,+50.0f},   {0.0f,1.0f}},//左下 インデックス0
+		{{ -50.0f,+50.0f,+50.0f},   {0.0f,0.0f}},//左上 インデックス1
+		{{ +50.0f,-50.0f,+50.0f},   {1.0f,1.0f}},//右下 インデックス2
+		{{ +50.0f,+50.0f,+50.0f},   {1.0f,0.0f}},//右上 インデックス3
 	};
 
 	//三角形のインデックスデータ
@@ -518,15 +518,38 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	}
 
 	//単位行列を追加
-	constMapTransform->mat = XMMatrixIdentity();
+	//constMapTransform->mat = XMMatrixIdentity();
+	//constMapTransform->mat.r[0].m128_f32[0]=2.0f/1280;//横幅
+	//constMapTransform->mat.r[1].m128_f32[1] = -2.0f/720;//縦幅
+	//constMapTransform->mat.r[3].m128_f32[0] = -1.0f;//-1平行移動
+	//constMapTransform->mat.r[3].m128_f32[1] = +1.0f;//+1平行移動
 
-	constMapTransform->mat.r[0].m128_f32[0]=2.0f/1280;//横幅
-	constMapTransform->mat.r[1].m128_f32[1] = -2.0f/720;//縦幅
-		
-	constMapTransform->mat.r[3].m128_f32[0]=-1.0f;//-1平行移動
-	constMapTransform->mat.r[3].m128_f32[1]=+1.0f;//+1平行移動
+	//並行投影行列の計算
+	constMapTransform->mat = XMMatrixOrthographicOffCenterLH
+	(
+		0.0f, 1280.0f,
+		720.0f, 0.0f,
+		0.0f, 1.0f
+	);
+	//透視投影行列の計算
+	constMapTransform->mat = XMMatrixPerspectiveFovLH(
+		XMConvertToRadians(45.0f),//上下画角45度
+		(float)1280 / 720,//アスペクト比(画面横幅/画面縦幅）
+		0.1f, 1000.0f//前端、奥端
+	);
+	//射影変換行列(透視投影)
+	XMMATRIX matProjection =
+		XMMatrixPerspectiveFovLH(
+			XMConvertToRadians(45.0f),//上下画角45度
+			(float)1280 / 720,//アスペクト比(画面横幅/画面縦幅）
+			1.0f, 1000.0f
+		);
 
+	//
+	
 
+	//定数バッファに転送
+	constMapTransform->mat = matProjection;
 
 
 	//ヒープ設定
@@ -952,7 +975,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		D3D12_GPU_DESCRIPTOR_HANDLE srvGpuHandle = srvHeap->GetGPUDescriptorHandleForHeapStart();
 		// SRVヒープの先頭にあるSRVをルートパラメータ1番に設定
 		commandList->SetGraphicsRootDescriptorTable(1, srvGpuHandle);
-        //インデックスバッファビューの設定コマンド
+		//インデックスバッファビューの設定コマンド
 		commandList->IASetIndexBuffer(&ibView);
 		// 定数バッファビュー(CBV)の設定コマンド
 		commandList->SetGraphicsRootConstantBufferView(0, constBufferMaterial->GetGPUVirtualAddress());
